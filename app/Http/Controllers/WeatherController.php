63a23305249fb94f209afe14a8e78fd4bc75d5a5
jasper-tambur <jasper.tambur@ametikool.ee>
2022-06-08 15:52:21 +0300

    wasdwa

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -11,13 +11,13 @@ class WeatherController extends Controller
 {
     public function Weather()
     {
-        $apiKey = env('API_KEY');
         $lat = 58.254951;
         $lon = 22.491898;
         if (Cache::has("weather")) {
             $data = Cache::get('weather');
         } else {
             $response = Http::get("https://api.openweathermap.org/data/2.5/weather?lat={$lat}&lon={$lon}&units=metric&appid=" . config("services.weather.key"));
+            Cache::put("weather", array("name" => $response["name"], "sys" => $response["sys"], "wind" => $response["wind"], "weather" => $response["weather"], "main" => $response["main"]));
             $data = Cache::get("weather");
         }
         return Inertia::render("Weather", [
